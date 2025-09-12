15533528ebde842f34d7182c8370ce7dd4c875e9
cee-aman <aman.kumar@crimsonenergy.ai>
2025-09-12 12:58:20 +0530

    modifed name for vercel

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,18 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),     tailwindcss()  ],
+  plugins: [react(), tailwindcss()],
+  
+  // Added to fix Netlify deployment errors
+  base: '/',  // ensures assets load correctly on Netlify
+  build: {
+    target: 'esnext',  // makes sure modern JS is used to avoid MIME type issues
+  },
+  server: {
+    https: true,        // fixes SockJS SecurityError locally
+    hmr: {
+      protocol: 'wss',  // secure WebSocket for HMR
+      host: 'localhost',
+    },
+  },
 })
